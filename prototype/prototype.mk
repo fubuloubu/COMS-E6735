505360c11c8be0d6d04622323feb0f334d9f3fe2
Bryant Eisenbach <fubuloubu@gmail.com>
2016-03-21 19:49:38 -0400

    Added line to compile target before running it to catch parsing errors

--- prototype/prototype.mk
+++ prototype/prototype.mk
@@ -2,7 +2,8 @@
 .PHONY: %.pyrun
 %.pyrun: %.py
 	@echo "  RUN xiwi $<"
-	@xiwi -F ./$< >> $@ 2>&1 && rm $@ || cat $@
+	@python -m py_compile $< # Compile to parse for errors
+	@xiwi -F ./$< 2> $@ && rm $@ || cat $@
 
 # clean rule for prototype scripts
 .PHONY: clean
