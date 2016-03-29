1390962b706ec19fd8400c29c1a808b43ed76bbb
Bryant Eisenbach <fubuloubu@gmail.com>
2016-03-28 22:20:05 -0400

    Rearranged alittle bit to only post tabulature when there is a guitar and hands identified

--- prototype/musicmodel.py
+++ prototype/musicmodel.py
@@ -5,12 +5,7 @@ import guitarmodel
 
 # Using hand and guitar models, determine if a note event occurs
 # and return that note, or a non-event ('-')
-def get_note(frame):
-    # Get the guitar's POI in the frame
-    guitar = guitarmodel.get_guitar(frame)
-    # Get the hand models for both hands
-    (picking_hand, fretting_hand) = handmodel.get_hands(frame)
-    
+def get_note(picking_hand, fretting_hand, guitar):
     # Determine if a note was plucked
     validnote = False
     if validnote:
@@ -24,21 +19,33 @@ def get_note(frame):
 global tablature 
 tablature = []
 def add_to_tablature(frame):
+    # Get the guitar's POI in the frame
+    guitar = guitarmodel.get_guitar(frame)
+    if not guitar:
+        raise ValueError("No guitar!")
+    # Get the hand models for both hands
+    (picking_hand, fretting_hand) = handmodel.get_hands(frame)
+    if not picking_hand or not fretting_hand:
+        raise ValueError("No hand(s)!")
+    
     global tablature 
-    tablature.append(get_note(frame))
+    tablature.append(get_note(picking_hand, fretting_hand, guitar))
     return tablature
 
 # Get tablature text (last N occurances)
 N = 20
 def get_tab(frame):
     # Get current tablature
-    current_tab = add_to_tablature(frame)
-    # Shorten if longer than N
-    if len(current_tab) > N:
-        current_tab = current_tab[-N:]
-    # Convert to text and add to screen
-    current_tab = '\n'.join([''.join(i) for i in zip(*current_tab)]) 
-    return "Tablature:\n" + current_tab
+    try:
+        current_tab = add_to_tablature(frame)
+        # Shorten if longer than N
+        if len(current_tab) > N:
+            current_tab = current_tab[-N:]
+        # Convert to text and add to screen
+        current_tab = '\n'.join([''.join(i) for i in zip(*current_tab)]) 
+        return "Tablature:\n" + current_tab
+    except ValueError:
+        return "No Guitar and/or Hand(s)!"
 
 def main(frame):
     # Flip the frame for ease of understanding
