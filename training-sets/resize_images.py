7d87edf9fd55681542d5926c8e3619375818a154
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-03 08:17:15 -0400

    Got the resize working appropiately

--- training-sets/resize_images.py
+++ training-sets/resize_images.py
@@ -6,13 +6,24 @@ def resize_image(image_file, new_size):
     image = Image.open(image_file)
     orig_size = image.size
     # compute largest aspect ratio
-    aspect_ratio = max(orig_size[0]/float(new_size[0]), float(orig_size[1]/new_size[1]))
+    aspect_ratio = max(orig_size[0]/float(new_size[0]), orig_size[1]/float(new_size[1]))
     # Scale size while retaining aspect ratio
-    corr_size = (int(orig_size[0]/aspect_ratio), int(orig_size[1]/aspect_ratio))
-    new_image = image.resize( corr_size )
-    # Fill in remaining space with black
-    color = 0 # Black
-    new_image = ImageOps.expand(new_image, new_size, color)
+    corr_size = (int(round(orig_size[0]/aspect_ratio)), int(round(orig_size[1]/aspect_ratio)))
+    image = image.resize( corr_size, Image.ANTIALIAS )
+    # Create image with black background
+    mode = image.mode
+    if len(mode) == 1:  # L, 1
+        new_background = (0)  
+    if len(mode) == 3:  # RGB
+        new_background = (0, 0, 0)
+    if len(mode) == 4:  # RGBA, CMYK
+        new_background = (0, 0, 0, 0) 
+    new_image = Image.new(mode, new_size, new_background)
+    x1 = (new_size[0] - corr_size[0])/2
+    y1 = (new_size[1] - corr_size[1])/2
+    x2 = x1 + corr_size[0]
+    y2 = y1 + corr_size[1]
+    new_image.paste(image, (x1,y1,x2,y2))
     new_image.save(image_file)
 
 if __name__ == '__main__':
@@ -26,12 +37,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     directory = args.directory[0]
     if not os.path.isdir(directory):
-        print "ERROR: No directory named " + directory + " exists!"
+        print("ERROR: No directory named " + directory + " exists!")
         sys.exit(1)
     else:
         jpg_files = glob.glob(directory + "/*.jpg")
         if len(jpg_files) == 0:
-            print "ERROR: No *.jpg files in directory " + directory
+            print("ERROR: No *.jpg files in directory " + directory)
             sys.exit(1)
         else:
             size = (args.W, args.H)
