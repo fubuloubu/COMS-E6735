d3747637e9cacbeac3232c10e1cda5179ecf82eb
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-04 23:58:05 -0400

    Reconfigured makefile to work with bibtex

--- documentation/documentation.mk
+++ documentation/documentation.mk
@@ -1,14 +1,30 @@
 # LaTeX makefile for documentation
 # Note: run it twice to ensure LastPage works
+# For Bibtex, run it 3 times for references
+PDFLATEX=pdflatex -shell-escape -interaction=batchmode
+BIBTEX=bibtex
+IGNORE_OUTPUT=/dev/null
+define GET_ERROR
+	(echo && echo "Error:" && echo && cat $*.$(1) | grep -A 10 ^! && rm $@ && exit 1)
+endef
+define RUN_PDFLATEX
+	echo " LATEX $<"; \
+	$(PDFLATEX) $< > $(IGNORE_OUTPUT) || $(call GET_ERROR,log);
+endef
+define RUN_BIBTEX
+	echo "BIBTEX $<"; \
+	$(BIBTEX) $* > $(IGNORE_OUTPUT) || $(call GET_ERROR,blg);
+endef
 .PHONY: %.pdf
 %.pdf: %.tex
-	@echo " LATEX $<"
-	@pdflatex -shell-escape -interaction=batchmode $< > /dev/null || \
-		(echo && echo "Error:" && echo && cat $*.log | grep -A 10 ^! && rm $@ && exit 1)
-	@bibtex $* > /dev/null || \
-		(echo && echo "Error:" && echo && cat $*.blg | grep -A 10 ^! && rm $@ && exit 1)
-	@pdflatex -shell-escape -interaction=batchmode $< > /dev/null || \
-		(echo && echo "Error:" && echo && cat $*.log | grep -A 10 ^! && rm $@ && exit 1)
+	@$(call RUN_PDFLATEX)
+	@if [ `grep "bibliography\.bib" $<` ]; then \
+		( \
+			$(call RUN_BIBTEX) \
+			$(call RUN_PDFLATEX) \
+		); \
+	fi
+	@$(call RUN_PDFLATEX)
 	@if [ -d "$$(readlink -f ~/Downloads)" ]; then \
 		echo "  MOVE $@"; \
 		mv $@ ~/Downloads; \
