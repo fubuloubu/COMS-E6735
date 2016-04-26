871c8edf55ed346ada08ef05b925904d7b083827
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-25 23:57:26 -0400

    Added test rule

--- prototype/prototype.mk
+++ prototype/prototype.mk
@@ -5,8 +5,18 @@
 	@python -m py_compile $< # Compile to parse for errors
 	@xiwi -T ./$< >/dev/null 2> $@ && cat $@ | awk '/Traceback/{flag=1}/Running exit commands/{flag=0}flag' && rm $@
 
+# rule for running canned input video and evaluating results
+%.pytest: %.mp4 | %.py
+	@echo " TEST $<"
+	./$| $< 2> $*.pyrun
+	@cat $*.pyrun | awk '/$|: Stats/{flag=1}/$|: Stats/{flag=0}flag' > $@
+	@python -c "import $*; $*.verify('$@');"
+	@rm $@
+
 # clean rule for prototype scripts
 .PHONY: clean
 clean:
 	@echo " CLEAN prototype"
 	@rm -f *.pyc
+	@rm -f *.pyrun
+	@rm -f *.pytest
