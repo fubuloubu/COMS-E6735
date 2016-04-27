6c123b33c1b92d3eaf39c94dbdb60f8b403e787e
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-26 21:08:29 -0400

    First attempt at getting this to work

--- prototype/handmodel.py
+++ prototype/handmodel.py
@@ -3,20 +3,21 @@ import utils
 import locate
 
 # Intepret hand area of frame into hand model
+# Handmodel: (x, y) pairs of fingertip points
 def to_handmodel(hand_image):
-    handmodel = []
+    handcontour = utils.skindetect(hand_image)
+    convex_pts = utils.convexhull(handcontour)
+    handmodel = [[75, 75]]
     return handmodel
-
-# Transform hand model to a wireframe for display
-def to_wireframe(handmodel):
-    wireframe = []
-    return wireframe
 
 # Get the hand models for both hands
 def get_hands(frame, pickhand_coords, frethand_coords):
     # Interpret hand areas to get hand models for both hands
     pickhand = to_handmodel(utils.crop(frame, pickhand_coords))
     frethand = to_handmodel(utils.crop(frame, frethand_coords))
+    # Uncrop hands to larger frame
+    pickhand = utils.reposition(pickhand, pickhand_coords)
+    frethand = utils.reposition(frethand, frethand_coords)
     return (pickhand, frethand)
 
 # Get digit wireframe of hand and present to user
@@ -24,14 +25,19 @@ def main(frame):
     # Flip the frame for ease of understanding
     frame = utils.mirror(frame)
     # Locate pickhand and frethand
-    (guitar_coords, pickhand_coords, frethand_coords) = 
-        locate.guitar_and_hands(frame)
-    # Get the hands in the frame
-    (pickhand, frethand) =
+    #(guitar_coords, pickhand_coords, frethand_coords) = \
+    #    locate.guitar_and_hands(frame)
+    pickhand_coords = [[100, 100, 250, 250]]
+    frethand_coords = [[400, 100, 550, 250]]
+    
+    # Get the hands (list of fingertips) in the frame
+    (pickhand, frethand) = \
         get_hands(frame, pickhand_coords, frethand_coords)
-    # Add wireframes for hands for display
-    frame = utils.addshape(frame, to_wireframe(pickhand))
-    frame = utils.addshape(frame, to_wireframe(frethand))
+    
+    # Add overlay circles on fingers for display
+    frame = reduce(utils.addcircle, [frame] + pickhand)
+    frame = reduce(utils.addcircle, [frame] + frethand)
+    
     return frame
 
 if __name__ == '__main__':
