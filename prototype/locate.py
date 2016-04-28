3ae29b3bba81f106ff62235ea71a9145803c1d7d
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-28 19:37:08 -0400

    Got rid of extraneous function calls and mirror

--- prototype/locate.py
+++ prototype/locate.py
@@ -3,33 +3,20 @@ import sys
 import utils
 progname = 'locate.py'
 
-# Find body of guitar for use in hand location algorithms
+# Initialize cascade classifiers
 guitar_cascade = utils.Cascade('guitar_classifier.xml')
-def guitar(frame):
-    guitar = guitar_cascade.detect(frame)
-    return guitar
-
-# Using guitar location in frame, find the picking hand location
 pickhand_cascade = utils.Cascade('pickhand_classifier.xml')
-def picking_hand(frame):
-    pickhand = pickhand_cascade.detect(frame)
-    return pickhand
-
-# Using guitar location in frame, find the fretting hand location
 frethand_cascade = utils.Cascade('frethand_classifier.xml')
-def fretting_hand(frame):
-    frethand = frethand_cascade.detect(frame)
-    return frethand
 
 # Main reterival function
 def guitar_and_hands(frame):
     # Find Coordinates of guitar
-    guitar_coords = guitar(frame)
+    guitar_coords = guitar_cascade.detect(frame)
     # Crop frame to guitar
     guitar_crop = utils.crop(frame, guitar_coords)
     # Get hands inside cropped guitar frame
-    pickhand_coords = picking_hand(guitar_crop)
-    frethand_coords = fretting_hand(guitar_crop)
+    pickhand_coords = pickhand_cascade.detect(guitar_crop)
+    frethand_coords = frethand_cascade.detect(guitar_crop)
     # Re-orient hand coordinates back to full frame
     pickhand_coords = utils.reposition(pickhand_coords, guitar_coords)
     frethand_coords = utils.reposition(frethand_coords, guitar_coords)
@@ -38,8 +25,6 @@ def guitar_and_hands(frame):
 # Apply above locate functions for POI,
 # and show them with bounded boxes for user
 def main(frame):
-    # Flip the frame for ease of understanding
-    frame = utils.mirror(frame)
     # Get guitar and hand bounding boxes
     (guitar_coords, pickhand_coords, frethand_coords) = \
         guitar_and_hands(frame)
