49cb9089688ddc5642fec008393a8f8c1bde4941
Bryant Eisenbach <fubuloubu@gmail.com>
2016-04-28 12:15:35 -0400

    Added skeletonization and line detection of fingers for finger tip points

--- prototype/handmodel.py
+++ prototype/handmodel.py
@@ -6,16 +6,29 @@ progname = 'handmodel.py'
 
 # Intepret hand area of frame into hand model
 # Handmodel: (x, y) pairs of fingertip points
-def to_handmodel(hand_image):
-    handcontour = utils.skindetect(hand_image)
-    handmodel = utils.convexhull(handcontour)
+def to_handmodel(hand_crop, direction='up'):
+    handcontour = utils.skindetect(hand_crop)
+    hand = utils.cut(hand_crop, handcontour)
+    handskeleton = utils.skeletonize(hand)
+    fingerlines = utils.linedetect(handskeleton)
+    if direction == 'dn':
+        handmodel = map(lambda l: [l[2], l[3]], fingerlines)
+    else:
+        handmodel = map(lambda l: [l[0], l[1]], fingerlines)
+    if len(handmodel) > 4:
+        handmodel = utils.cluster(handmodel, \
+                origin=[0, 0], \
+                distance=lambda p1, p2: (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2, \
+                compare=lambda p1, p2, cp: cp(cp(p1[0],p2[0]), cp(p1[1],p2[1])), \
+                combine=lambda p1, p2: [(p1[0]+p2[0])/2, (p1[1]+p2[1])/2], \
+                K=4)
     return handmodel
 
 # Get the hand models for both hands
 def get_hands(frame, pickhand_coords, frethand_coords):
     # Interpret hand areas to get hand models for both hands
-    pickhand = to_handmodel(utils.crop(frame, pickhand_coords))
-    frethand = to_handmodel(utils.crop(frame, frethand_coords))
+    pickhand = to_handmodel(utils.crop(frame, pickhand_coords), 'dn')
+    frethand = to_handmodel(utils.crop(frame, frethand_coords), 'up')
     # Uncrop hands to larger frame
     pickhand = map(lambda p: utils.reposition(p, pickhand_coords), pickhand)
     frethand = map(lambda p: utils.reposition(p, frethand_coords), frethand)
