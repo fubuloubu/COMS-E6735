de1784557b6d86a327bdf66f1e9be8cf3f7765eb
Bryant Eisenbach <fubuloubu@gmail.com>
2016-05-02 22:39:33 -0400

    Got pytest rule working

--- prototype/prototype.mk
+++ prototype/prototype.mk
@@ -1,22 +1,64 @@
+# All the targets we have
+TARGETS=locate.results handmodel.results guitarmodel.results musicmodel.results
+
+PYTHON=python
+
+SHOW=xiwi -T
+IGNORE=/dev/null
+EXECUTE=./$(1) $(2) 2>$(4) > $(3)
+
+AWK=awk
+ERROR_FILTER='/Traceback/{flag=1}/Running exit commands/{flag=0}flag'
+CHECK_ERROR=$(AWK) $(ERROR_FILTER)
+
 # rule to run python scripts
 .PHONY: %.pyrun
 %.pyrun: %.py
-	@echo "  RUN xiwi $<"
-	@python -m py_compile $< # Compile to parse for errors
-	@xiwi -T ./$< >/dev/null 2> $@ && cat $@ | awk '/Traceback/{flag=1}/Running exit commands/{flag=0}flag' && rm $@
-
-# rule for running canned input video and evaluating results
-%.pytest: %.mp4 | %.py
-	@echo " TEST $<"
-	./$| $< 2> $*.pyrun
-	@cat $*.pyrun | awk '/$|: Stats/{flag=1}/$|: Stats/{flag=0}flag' > $@
-	@python -c "import $*; $*.verify('$@');"
+	@echo "  RUN $<"
+	@$(SHOW) $(call EXECUTE,$<,,$(IGNORE),$@)
+	@cat $@ | $(CHECK_ERROR)
 	@rm $@
+
+STATS_FILTER='/$(1): Stats/{flag=1}/$(1): Stats/{flag=0}flag'
+FILTER_STATS=$(AWK) $(STATS_FILTER)
+#@cat $$@ | $(call FILTER_STATS,$$<) > $$@
+
+# Cat all of them together
+VIDEO_FILES=$(shell ls *.mp4)
+
+# Rule for running canned input video and evaluating results
+define TEST_VIDEO_RULES
+%.$(1).pytest: %.py
+	@echo " TEST $$< > $(1)"
+	@$(call EXECUTE,$$<,$(1),$(IGNORE),$$@)
+
+%.$(1).results: %.$(1).pytest
+	@echo "  GEN $$@"
+	@$(PYTHON) -c "import $$*; $$*.verify('$$<');" > $$@
+endef
+# Generate a rule for each video
+$(foreach video,$(VIDEO_FILES),$(eval $(call TEST_VIDEO_RULES,$(video))))
+
+# Collate all the video results files into one
+%.results: $(foreach video,$(VIDEO_FILES),%.$(video).results ) | %.py
+	@echo "  GEN $@"
+	@touch $@
+	@$(foreach rslt,$^,cat $(rslt) >> $@;)
+
+# Collate all the results files into one table
+METRICS_SCRIPT=results_evaluation.py
+METRICS_OPTIONS  = -m *+score total+score
+METRICS_OPTIONS += -f booktabs_latex
+GET_METRICS=$(PYTHON) $(METRICS_SCRIPT) $(METRICS_OPTIONS)
+results.tex: $(foreach target,$(TARGETS),$(subst py,results,$(target)))
+	@echo "  GEN $@"
+	@$(GET_METRICS) -t $^
 
 # clean rule for prototype scripts
 .PHONY: clean
 clean:
-	@echo " CLEAN prototype"
+	@echo "CLEAN prototype"
 	@rm -f *.pyc
 	@rm -f *.pyrun
 	@rm -f *.pytest
+	@rm -f *.results
